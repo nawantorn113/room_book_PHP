5e6802b420efdb55b3b0029604822e0d0fa143c0
nawantorn113 <stu6512732113@sskru.ac.th>
2025-10-07 15:37:00 +0700

    commit

--- books_list3.php
+++ books_list3.php
@@ -14,7 +14,7 @@ if (isset($_GET['rmid'])) {
 
 <head>
     <meta charset="UTF-8">
-    <title>ระบบจองห้องพัก</title>
+    <title>ระบบจองห้องพัก<>/center></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
 </head>
